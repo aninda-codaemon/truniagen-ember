f6268275ed81e636e2d95b74c1ad273713558da1
Aninda Kar <aninda.kar@codaemonsoftwares.com>
2017-11-27 17:39:32 +0530

    Checkout route added

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('home', {path: '/'});
   this.route('who-we-are');
+  this.route('checkout');
 });
 
 export default Router;
